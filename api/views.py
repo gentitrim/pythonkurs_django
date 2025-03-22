cc9b5496a0a4acb8f1a37a3d74d39d05158020f0
gentitrim <gentitrim1989@gmail.com>
2025-03-22 13:52:18 +0100

    serializer

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin,CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin
 
 # Create your views here.
 
